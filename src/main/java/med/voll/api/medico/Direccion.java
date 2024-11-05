0f10a9164b1976753deadd450e1ba76ac9453892
Jeffrey MC <jeffmc@duck.com>
2024-11-04 18:30:40 -0600

    databased connected and table created

---
+++ src/main/java/med/voll/api/medico/Direccion.java
@@ -0,0 +1,27 @@
+package med.voll.api.medico;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.direccion.DatosDireccion;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Direccion {
+    private String calle;
+    private String distrito;
+    private String ciudad;
+    private String numero;
+    private String complemento;
+
+    public Direccion(DatosDireccion direccion) {
+        this.calle = direccion.calle();
+        this.distrito = direccion.distrito();
+        this.numero = direccion.numero();
+        this.ciudad = direccion.ciudad();
+        this.complemento = direccion.complemento();
+    }
+}
---
+++ src/main/resources/db/migration/V1__create-table-medicos.sql
@@ -0,0 +1,12 @@
+create table medicos(
+  id BIGSERIAL PRIMARY KEY,
+    nombre varchar(100) not null,
+    email varchar(100) not null unique,
+    documento varchar(6) not null unique,
+    especialidad varchar(100) not null,
+    calle varchar(100) not null,
+    distrito varchar(100) not null,
+    complemento varchar(100) not null,
+    numero varchar(20),
+    ciudad varchar(100) not null
+);
