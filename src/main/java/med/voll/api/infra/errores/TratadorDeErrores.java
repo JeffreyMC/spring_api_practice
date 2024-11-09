2ef09f715cb8e45874cf2aa67b04815c2d100d60
Jeffrey MC <jeffmc@duck.com>
2024-11-09 13:34:39 -0600

    Login credentials and security working. User table created

--- src/main/java/med/voll/api/infra/errores/TratadorDeErrores.java
+++ src/main/java/med/voll/api/infra/errores/TratadorDeErrores.java
@@ -1,11 +1,10 @@
-package med.voll.api.infra;
+package med.voll.api.infra.errores;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
---
+++ src/main/resources/db/migration/V3__create-table-usuarios.sql
@@ -0,0 +1,6 @@
+create table usuarios(
+  id BIGSERIAL PRIMARY KEY,
+    username varchar(100) not null,
+    password varchar(300) not null
+
+);
